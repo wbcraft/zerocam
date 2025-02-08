fe2fa4d25cba9dd6473e30813eadd21fbe2dbe0f
wbcraft <164401157+wbcraft@users.noreply.github.com>
2025-02-07 23:13:47 -0600

    Update cam.sh
    Changed capture output to mp4
    Changed name structure in the video conversion
    Changed what's deleted at the end.

--- cam.sh
+++ cam.sh
@@ -11,10 +11,10 @@
 # --output = date&time.h264
 # --tuning-file is based on your sensor.  For this purpose, I'm using the Camera Module 3.
 
-libcamera-vid -t 3601s --bitrate 6000000 --autofocus-mode manual --width 1280 --height 720 --framerate 30 --nopreview --output $(date +%Y%m%d_%H%M.h264) --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx708.json
+libcamera-vid -t 3601s --bitrate 6000000 --autofocus-mode manual --width 1280 --height 720 --framerate 30 --nopreview --output $(date +%Y%m%d_%H%M.mp4) --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx708.json
 
 # Video Conversion
-for video in `ls *.h264`; do ffmpeg -framerate 30 -i $video -c copy $video\ converted.mp4; done
+for video in `ls *.mp4`; do ffmpeg -framerate 30 -i $video -c copy "new_$video"; done
 
 # Let's free up space by removing the original file.  Will remove this section later in the Python version
-rm -rf *.h264
+rm -rf 2025*
