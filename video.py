03dc59f38e15ee57b5f8ed5863657d3a73e96a71
wbcraft <164401157+wbcraft@users.noreply.github.com>
2025-02-15 00:56:38 -0600

    Create video.py
    A quick working python script to record for 10 seconds.  Adjust in the time.sleep function.

---
+++ video.py
@@ -0,0 +1,17 @@
+from picamera2.encoders import H264Encoder, Quality
+from picamera2 import Picamera2, MappedArray
+from picamera2.outputs import FfmpegOutput
+import time
+
+
+picam2 = Picamera2()
+#config = picam2.create_configuration(width=1920, height=1080)
+video_config = picam2.create_video_configuration(main={"size":(1920,1080)})
+picam2.configure(video_config)
+
+encoder = H264Encoder(bitrate=8000000)
+output = FfmpegOutput("/mnt/tgz/recordings/pi5cam.mp4", audio=False)
+
+picam2.start_recording(encoder, output, quality=Quality.VERY_HIGH)
+time.sleep(10)
+picam2.stop_recording()
