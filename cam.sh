04b905d31da8e5d685966ad25fd7dbd3f23d554d
wbcraft <164401157+wbcraft@users.noreply.github.com>
2025-02-15 21:05:36 -0600

    Update cam.sh
    A different way to do this with variables, an if statement checking for the NFS server, etc.  Works the same as before but will auto write to the storage and run ffmpeg against that file on the NFS storage.

--- cam.sh
+++ cam.sh
@@ -8,48 +8,67 @@
 # --framerate should be 30 max for our purposes.  24 is my use case.
 # --nopreview means it won't try to show it on a screen.
 # --output = defined in the raw variable.
-# --tuning-file is based on your sensor.  For this purpose, I'm using the Camera Module 3. 
+# --tuning-file is based on your sensor.  For this purpose, I'm using the Camera Module 3.
 
 # First, let's define our working directory so it works in cron.
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-# Let's make a variable for the raw file.
+# Let's define a couple variables first...
 raw=$(date +%Y%m%d_%H%M).raw
+mount=$(mount | grep nfs)
+server=/mnt/tgz/recordings
 
 # Run libcamera with these options, defined above.
 #
-# I can't write directly to the server or all processing would be done there.  The Zero 2 Wi-Fi connection (in my case) is too slow to stream (write) 1080p@24 recorded at 6k bitrate.  
+# I can't write directly to the server or all processing would be done there.  The Zero 2 Wi-Fi connection (in my case) is too slow to stream (write) 1080p@24 recorded at 6k bitrate.
+# 720p@30 may be more suitable and will be tested.
+if [ -z "$mount" ]; then
+        printf "NFS mount not found.  Running locally\n\n"
+
+# Run libcamera with these options, defined above.
+#
+# I can't write directly to the server or all processing would be done there.  The Zero 2 Wi-Fi connection (in my case) is too slow to stream (write) 1080p@24 recorded at 6k bitrate.
 # 720p@30 may be more suitable and will be tested.
 
-libcamera-vid -t 3601s --bitrate 6000000 --autofocus-mode auto --width 1920 --height 1080 --framerate 24 --nopreview --output $raw --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx708.json
+        libcamera-vid -t 10s --bitrate 8000000 --width 1920 --height 1080 --framerate 24 --nopreview --output $raw --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx708.json
 
 # Video Conversion -- match the framerate in the libcamera-vid command above.
-for video in `ls $raw`
-  do
-    ffmpeg -framerate 24 -i $video -c copy "$video.mp4"
-  done
+        for video in `ls $raw`
+          do
+            ffmpeg -framerate 24 -i $video -c copy "$video.mp4"
+          done
 
 # Strip out the .raw to make the file name cleaner
-for rename in `ls *.mp4`
-  do
-    mv "$rename" "`echo $rename | sed 's/.raw//'`"
-  done
+        for rename in `ls *.mp4`
+          do
+            mv "$rename" "`echo $rename | sed 's/.raw//'`"
+            rm $raw
+          done
 
-# This checks to see if the NFS mount is available (for server storage). If there isn't a mount, it dies.  If there is a mount, it'll rsync the files, remove the local files, then quit.
-# You can comment the mount command and uncomment the SERVER and ping lines if you wish to check network instead.  Edit the rsync line for your use case.
+else
+        printf "NFS Available. Writing to Server!\n\n"
 
-#SERVER=10.0.0.1
-for i in `ls *.mp4`; do
-#  /usr/bin/ping -c 1 $SERVER &> /dev/null
-   /usr/bin/mount | grep nfs &> /dev/null
-    if [[ $? -ne 0 ]]; then
-      /usr/bin/printf "Couldn't rsync - files not deleted"
-    else
-      /usr/bin/rsync -avu --ignore-existing $i /mnt/tgz/recordings/
-      /usr/bin/sleep 3
-      /usr/bin/rm -rf $raw *.mp4
-      /usr/bin/printf "\n\nLocal files deleted\n\n"
+        
+# Run libcamera with these options, defined above.
+# Quick variable for the server mount point.
+
+        libcamera-vid -t 10s --bitrate 8000000 --width 1920 --height 1080 --framerate 24 --nopreview --output $server/$raw --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx708.json
+
+# Video Conversion -- match the framerate in the libcamera-vid command above.
+        for video in `ls $server/$raw`
+          do
+            ffmpeg -framerate 24 -i $video -c copy "$video.mp4"
+          done
+
+# Strip out the .raw to make the file name cleaner
+        for rename in `ls $server/*.raw.mp4`
+          do
+            mv "$rename" "`echo $rename | sed 's/.raw//'`"
+          done
+#  This line is no longer needed
+#  /usr/bin/rsync -avu --ignore-existing $i /mnt/tgz/recordings/
+/usr/bin/sleep 3
+/usr/bin/rm -rf $server/$raw
+/usr/bin/printf "\n\nRaw files deleted\n\n"
     fi
-done
-
 
